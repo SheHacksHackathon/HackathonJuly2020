181365bfacc824f302a26ac3a60bf3a4ddaf56f9
Janis Rancourt <janis.rancourt@gmail.com>
2020-07-21 16:22:22 -0400

    prepping frontend client with basic components and setup

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Login() {
+ return (
+  <div>
+   <form>
+    <input
+     type="text"
+     name=""
+     id=""
+     placeholder="username"
+    />
+   </form>
+  </div>
+ )
+}
