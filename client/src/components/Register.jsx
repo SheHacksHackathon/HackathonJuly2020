4231c8a739ff7c7cf5310d1baf68f654e9b4edee
Janis Rancourt <janis.rancourt@gmail.com>
2020-07-22 11:16:49 -0400

    adding some css for the design

---
+++ client/src/components/Register.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+import { withRouter } from 'react-router-dom'
+class Register extends Component {
+ state = {
+  username: "",
+  password: ""
+ }
+
+ handleChange = (e) => {
+  const { name, value } = e.target;
+  this.setState({
+   [name]: value
+  })
+ }
+
+ handleSubmit = (e) => {
+  e.preventDefault()
+  // this.props.handleRegisterSubmit(this.state)
+  this.setState({
+   username: "",
+   password: ""
+  })
+  this.props.history.push('/')
+ }
+
+ render() {
+  return (
+   <div className="SignUp">
+    <h2>Sign Up</h2>
+    <form
+     onSubmit={this.handleSubmit}
+     className="sign-up-form"
+    >
+
+     <input
+      type="text"
+      name="username"
+      value={this.state.username}
+      onChange={this.handleChange}
+      placeholder="username"
+     />
+     <input
+      type="password"
+      name="password"
+      value={this.state.password}
+      onChange={this.handleChange}
+      placeholder="password"
+     />
+     <Button
+      // handleClick={this.handleSubmit}
+      buttonText="REGISTER"
+     />
+
+    </form>
+   </div>
+  )
+ }
+}
+export default withRouter(Register)
