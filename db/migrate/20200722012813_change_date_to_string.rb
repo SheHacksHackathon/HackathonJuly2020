86e762b456fee75bb4f19f04c5d3a426b87f97a1
Janis Rancourt <janis.rancourt@gmail.com>
2020-07-23 17:23:38 -0400

    updates to db

--- db/migrate/20200722012813_change_date_to_string.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDateToString < ActiveRecord::Migration[6.0]
-  def change
-    change_column :tickets, :event_date, :string
-    change_column :tickets, :purchase_date, :string
-  end
-end
