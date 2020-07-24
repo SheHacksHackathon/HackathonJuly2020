f8fe9eee5e2ce3169cd777a52ed64fef7793e5f1
Janis Rancourt <janis.rancourt@gmail.com>
2020-07-24 11:16:16 -0400

    adding back bar for ticket view

--- db/migrate/20200722030814_add_name_to_ticket.rb
+++
@@ -1,6 +0,0 @@
-class AddNameToTicket < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tickets, :fname, :string
-    add_column :tickets, :lname, :string
-  end
-end
--- db/migrate/20200722193051_add_field_to_ticket.rb
+++
@@ -1,6 +0,0 @@
-class AddFieldToTicket < ActiveRecord::Migration[6.0]
-  def change
-    add_column :tickets, :check_in, :string
-    add_column :tickets, :check_out, :string
-  end
-end
